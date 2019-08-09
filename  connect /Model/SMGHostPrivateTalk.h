5d9174ebf0dd30746e6d63b2e5f3734a66ddf204
duan <dalizhiyan@@qq.com>
2019-08-09 09:18:49 +0800

    =

---
+++  connect /Model/SMGHostPrivateTalk.h
@@ -0,0 +1,21 @@
+//
+//  SMGHostPrivateTalk.h
+//  DSXLive
+//
+//  Created by 沈树亮 on 2019/8/9.
+//  Copyright © 2019 xiong. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+// 主编私聊
+@interface SMGHostPrivateTalk : NSObject
+
+- (void)openTalk;
+- (void)closeTalk;
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++  connect /Model/SMGHostPrivateTalk.m
@@ -0,0 +1,121 @@
+//
+//  SMGHostPrivateTalk.m
+//  DSXLive
+//
+//  Created by 沈树亮 on 2019/8/9.
+//  Copyright © 2019 xiong. All rights reserved.
+//
+
+#import "SMGHostPrivateTalk.h"
+#import <VSRTC/VSRTC.h>
+#import <VSRTC/VSMedia.h>
+#import <VSRTC/VSRoomUser.h>
+
+@interface SMGHostPrivateTalk ()<VSMediaEventHandler>
+
+@property (nonatomic, strong) VSMedia *media;
+
+@end
+
+@implementation SMGHostPrivateTalk
+
+- (instancetype)init {
+    if (self = [super init]) {
+        [self setup];
+    }
+    return self;
+}
+
+- (VSMedia *)media {
+    if (!_media) {
+        NSArray *users = [[VSRTC sharedInstance] getMemberList];
+        //    VSRoomUser *user = [[VSRTC sharedInstance] getSession];
+        for (VSRoomUser *user in users) {
+            NSDictionary *dic = user.custom;
+            NSDictionary *extend = dic[@"extend"];
+            NSString *room_active_role = extend[@"room_active_role"];
+            
+            if ([user stream_id] && [room_active_role containsString:@"host"]) {
+                VSMedia *media = [[VSRTC sharedInstance] FindRemoteMedia:[user stream_id]];
+                if (!media) {
+                    media = [[VSRTC sharedInstance] CreateRemoteMedia:[user stream_id] reuseExist:YES];
+                }
+                [media SetEventHandler:self];
+                [media OpenWithVideo:NO andAudio:YES];
+                _media = media;
+            }
+        }
+    }
+    return _media;
+}
+
+- (void)setup {
+    [self media];
+}
+
+- (void)openTalk {
+             [media Publish:NO streamBitrate:_streamBitrate stramLabel:user.userId];
+}
+- (void)closeTalk;
+
+#pragma mark - updateStreming
+- (void)updateStram {
+    VSMedia *media = self.media;
+    
+    if (media.stream_state == VS_MEDIA_STREAM_IDLE) {
+        if (media.type == VS_MEDIA_CAPTURE) {
+            [[VSRTC sharedInstance] updateStream:0 withType:YES];
+        } else {
+            [[VSRTC sharedInstance] updateStream:0 withType:NO];
+        }
+    } else if (media.stream_state == VS_MEDIA_STREAM_STARTED) {
+        if (media.type == VS_MEDIA_CAPTURE) {
+            [[VSRTC sharedInstance] updateStream:media.stream_id withType:YES];
+        } else {
+            [[VSRTC sharedInstance] updateStream:media.stream_id withType:NO];
+        }
+    } else {
+        
+    }
+}
+
+#pragma mark - VSMediaEventHandler
+- (void)OnOpening {
+    
+}
+
+- (void)OnOpened {
+    
+}
+
+- (void)OnClose {
+    
+}
+
+- (void)OnError:(NSString *)errDesc {
+    
+}
+
+- (void)OnStreamIdle {
+    [self updateStram];
+}
+
+- (void)OnStreamStarting {
+    [self updateStram];
+}
+
+- (void)OnStreamStarted:(uint64_t)streamId {
+    //    VSMedia *captureMedia = [[VSRTC sharedInstance] CreateCaptureMedia];
+    //    NSLog(@"<<<<<<<<<< %@", [captureMedia getStats]);
+    NSLog(@"<<<<<<<<<<>>>>>>>>>>>>>>  %llu", streamId);
+    [self updateStram];
+}
+
+- (void)OnStreamRestarting {
+    
+}
+- (void)OnStreamFailed {
+    
+}
+
+@end
